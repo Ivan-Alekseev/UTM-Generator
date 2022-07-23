acd7cd5da54729cc9a07771efaa5fb2d0a91a08c
My NameDafat220195git config --global user.email Dafat220195@yandex.r <Dafat220195@yandex.ru>
2022-07-23 13:37:02 +0300

    Change background rotation requestAnimationFrame

--- docs/js/animation.js
+++ docs/js/animation.js
@@ -1,13 +1,19 @@
 let animationCtrl = function () {
         let rotationDeg = 0;
-        setInterval(rotation, 10);
+        //setInterval(rotation, 10);
+        let startTime = new Date ().getTime();
         function rotation () {
-        
-        rotationDeg += 0.5;
-        
-        document.querySelector(".main").style.background = 
-        `linear-gradient(${rotationDeg}deg, #7F7FD5, #86A8E7,#91EAE4)`;
-    }
+                            let currTime = new Date ().getTime();
+                            
+                            rotationDeg = 1 + ((currTime - startTime)/1000*30);
+
+                            document.querySelector(".main").style.background = 
+                            `linear-gradient(${rotationDeg}deg, #7F7FD5, #86A8E7,#91EAE4)`;
+                            requestAnimationFrame (rotation);
+                            
+                            
+        }
+        rotation()
         
 }
 //События анимации фона - ajy 
